2e317d5f990bc60365d777cd73f23bbb7d966e89
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-02-25 00:22:22 -0300

    feat: Concluido os grids para exibição dos cards

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import * as C from "./App.styles";
+import logoImage from "./assets/devmemory_logo.png";
+import RestartIcon from "./svgs/restart.svg";
+import Button from "./components/Button";
+import InfoItem from "./components/InfoItem/Index";
+import { GridItemType } from "./types/GridItemType";
+import { items } from "./data/items";
+
+export default function App() {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => {
+    resetAndCreateGrid();
+  }, []);
+
+  const resetAndCreateGrid = () => {
+    // passo 1 - resetar o jogo
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShownCount(0);
+
+    // passo 2 - criar o grid e preencher
+    // passo 2.1 - criar grid vazio
+    let tmpGrid: GridItemType[] = [];
+    for (let i = 0; i < items.length * 2; i++) {
+      tmpGrid.push({
+        item: null,
+        shown: false,
+        permanentShown: false,
+      });
+    }
+
+    // passo 2.2 - preencher o grid aleatoriamente
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < items.length; j++) {
+        let pos = -1;
+        while (pos < 0 || tmpGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        tmpGrid[pos].item = j;
+      }
+    }
+
+    // passo 2.3 - jogar no state
+    setGridItems(tmpGrid);
+
+    // passo 3 - começar o jogo
+    setPlaying(true);
+  };
+
+  return (
+    <C.Container>
+      <C.Info>
+        <C.LogoLink>
+          <img src={logoImage} width="200" alt=""></img>
+        </C.LogoLink>
+        <C.InfoArea>
+          <InfoItem label="Tempo" value="00:00" />
+          <InfoItem label="Movimentos" value="0" />
+        </C.InfoArea>
+        <Button
+          label="Reiniciar"
+          icon={RestartIcon}
+          onClick={resetAndCreateGrid}
+        />
+      </C.Info>
+      <C.GridArea>...</C.GridArea>
+    </C.Container>
+  );
+}
